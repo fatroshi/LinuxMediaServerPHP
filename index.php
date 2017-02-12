779680f04e0e2a17dbf49ba0f75146e274c7850a
fatroshi <fa@neodesign.se>
2017-02-12 20:13:14 +0100

    avbar-fixed-top

--- index.php
+++ index.php
@@ -108,28 +108,87 @@ include_once("APP/Controller.php");
             $(".addCategory" ).toggle( flipNewCategory++ % 2 === 0 );
         });
 
+        var flipTv = 0;
+        $(".tv").hide();
+
+        $(".tvControl").click(function(event){
+            $(".tv" ).toggle( flipTv++ % 2 === 0 );
+        });
+
     });
 </script>
 
+<nav class="navbar navbar-default navbar-fixed-top">
+        <center>
+            <span class="h2">
 
-<div class="page-header">
-    <h1><a href="index.php">Atroshi</a> <small>Cloud</small></h1>
-</div>
+                <a href="index.php"><span class="h1">Atroshi</span></a> <span class="h2">Cloud</span>
 
-<div class="menu">
-    <button type="button" class="btn btn-default downloadControl " aria-label="Left Align" >
-        <span class="glyphicon glyphicon-download" ></span>
-    </button>
+                <button type="button" class="btn btn-default downloadControl " aria-label="Left Align" >
+                    <span class="glyphicon glyphicon-download" ></span>
+                </button>
 
-    <button type="button" class="btn btn-default categoryControl " aria-label="Left Align" >
-        <span class="glyphicon glyphicon-plus" ></span>
-    </button>
+                <button type="button" class="btn btn-default categoryControl " aria-label="Left Align" >
+                    <span class="glyphicon glyphicon-plus" ></span>
+                </button>
 
-    <button type="button" class="btn btn-default mediaControl " aria-label="Left Align" >
-        <span class="glyphicon glyphicon-play" ></span>
-    </button>
-</div>
+                <button type="button" class="btn btn-default mediaControl " aria-label="Left Align" >
+                    <span class="glyphicon glyphicon-play" ></span>
+                </button>
 
+                <button type="button" class="btn btn-default tvControl " aria-label="Left Align" >
+                    <span class="glyphicon glyphicon-blackboard" ></span>
+                </button>
+
+            </span>
+
+            <div class="control alert alert-info">
+
+                <button type="button" class="btn btn-default  " aria-label="Left Align" id="volymUp">
+                    <span class="glyphicon glyphicon-volume-down" ></span>
+                </button>
+
+                <button type="button" class="btn btn-default " aria-label="Left Align" id="volymDown">
+                    <span class="glyphicon glyphicon-volume-up" ></span>
+                </button>
+
+                <button type="button" class="btn btn-default " aria-label="Left Align" id="quit">
+                    <span class="glyphicon glyphicon-glyphicon glyphicon-off" ></span>
+                </button>
+
+                <hr>
+
+                <button type="button" class="btn btn-default seek" aria-label="Left Align" id="-20">
+                    <span class="glyphicon glyphicon-fast-backward " ></span>
+                </button>
+
+                <button type="button" class="btn btn-default seek" aria-label="Left Align" id="-5">
+                    <span class="glyphicon glyphicon-step-backward " ></span>
+                </button>
+
+                <button type="button" class="btn btn-default " aria-label="Left Align" id="resume">
+                    <span class="glyphicon glyphicon-play" > </span>
+                </button>
+
+                <button type="button" class="btn btn-default " aria-label="Left Align" id="pause">
+                    <span class="glyphicon glyphicon-pause" > </span>
+                </button>
+
+                <button type="button" class="btn btn-default seek" aria-label="Left Align" id="5">
+                    <span class="glyphicon glyphicon-step-forward " ></span>
+                </button>
+
+                <button type="button" class="btn btn-default seek" aria-label="Left Align" id="20">
+                    <span class="glyphicon glyphicon-fast-forward " ></span>
+                </button>
+
+            </div>
+
+
+        </center>
+</nav>
+
+</br>
 </br>
 
 <div class="well download">
@@ -190,47 +249,7 @@ if(isset($_POST['newCategory']) && $_POST['name'] != "" && $_FILES['image'] !=""
 </div>
 
 
-<div class="control alert alert-info">
 
-    <button type="button" class="btn btn-default  " aria-label="Left Align" id="volymUp">
-        <span class="glyphicon glyphicon-volume-down" ></span>
-    </button>
-
-    <button type="button" class="btn btn-default " aria-label="Left Align" id="volymDown">
-        <span class="glyphicon glyphicon-volume-up" ></span>
-    </button>
-
-    <button type="button" class="btn btn-default " aria-label="Left Align" id="quit">
-        <span class="glyphicon glyphicon-glyphicon glyphicon-off" ></span>
-    </button>
-
-    <hr>
-
-    <button type="button" class="btn btn-default seek" aria-label="Left Align" id="-20">
-        <span class="glyphicon glyphicon-fast-backward " ></span>
-    </button>
-
-    <button type="button" class="btn btn-default seek" aria-label="Left Align" id="-5">
-        <span class="glyphicon glyphicon-step-backward " ></span>
-    </button>
-
-    <button type="button" class="btn btn-default " aria-label="Left Align" id="resume">
-        <span class="glyphicon glyphicon-play" > </span>
-    </button>
-
-    <button type="button" class="btn btn-default " aria-label="Left Align" id="pause">
-        <span class="glyphicon glyphicon-pause" > </span>
-    </button>
-
-    <button type="button" class="btn btn-default seek" aria-label="Left Align" id="5">
-        <span class="glyphicon glyphicon-step-forward " ></span>
-    </button>
-
-    <button type="button" class="btn btn-default seek" aria-label="Left Align" id="20">
-        <span class="glyphicon glyphicon-fast-forward " ></span>
-    </button>
-
-</div>
 
 
 
